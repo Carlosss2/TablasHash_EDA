7ebb4239378e4ceea055e52897ad0329f32e65f9
Carlosss2 <233286@ids.upchiapas.edu.mx>
2024-07-25 23:48:41 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,108 @@
+import models.HashTable;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        Scanner scanner = new Scanner(System.in);
+        HashTable hashTable1 = new HashTable(1000);
+        HashTable hashTable2 = new HashTable(1000);
+
+        String line;
+        String splitBy = ",";
+        int id = 1;
+
+        try (BufferedReader br = new BufferedReader(new FileReader("bussines.csv"))) {
+            while ((line = br.readLine()) != null) {
+                String[] business = line.split(splitBy);
+                if (business.length >= 5) {
+                    String key = business[1];
+                    String value = "ID=" + business[0] + ", Address=" + business[2] +
+                            ", City=" + business[3] + ", State=" + business[4];
+
+                    long time1 = hashTable1.measurePutTime(key, value, 1);
+                    long time2 = hashTable2.measurePutTime(key, value, 2);
+
+                    System.out.println("[" + id + "] Business [ID=" + business[0] + ", Name=" +
+                            business[1] + ", Address=" + business[2] + ", City=" +
+                            business[3] + ", State=" + business[4] + "]");
+                    System.out.println("Tiempo para HashFunction1: " + time1 + " ns");
+                    System.out.println("Tiempo para HashFunction2: " + time2 + " ns");
+                    id++;
+                } else {
+                    System.err.println("Saltando línea inválida: " + line);
+                }
+            }
+
+
+            boolean exit = false;
+            do {
+                System.out.println("Menu:");
+                System.out.println("1. Buscar por clave");
+                System.out.println("2. Mostrar datos por índice");
+                System.out.println("3. Salir");
+                System.out.print("Selecciona una opción: ");
+                int option = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (option) {
+                    case 1:
+                        System.out.print("Introduce la Clave a Buscar: ");
+                        String searchKey = scanner.nextLine().trim();
+
+                        String foundValue1 = hashTable1.searchValueByKey(searchKey, 1);
+                        String foundValue2 = hashTable2.searchValueByKey(searchKey, 2);
+
+                        if (foundValue1 != null || foundValue2 != null) {
+                            if (foundValue1 != null) {
+                                System.out.println("Clave '" + searchKey + "' encontrada en Tabla Hash 1. " + foundValue1);
+                            }
+                            if (foundValue2 != null) {
+                                System.out.println("Clave '" + searchKey + "' encontrada en Tabla Hash 2. " + foundValue2);
+                            }
+                        } else {
+                            System.out.println("Clave '" + searchKey + "' no encontrada.");
+                        }
+                        break;
+
+                    case 2:
+                        System.out.print("Introduce el índice para mostrar datos: ");
+                        int searchIndex = scanner.nextInt();
+                        scanner.nextLine();
+
+                        System.out.println("Datos en el índice " + searchIndex + " de la Tabla Hash 1:");
+                        List<String> data1 = hashTable1.getDataAtIndex(searchIndex);
+                        for (String data : data1) {
+                            System.out.println(data);
+                        }
+
+                        System.out.println("Datos en el índice " + searchIndex + " de la Tabla Hash 2:");
+                        List<String> data2 = hashTable2.getDataAtIndex(searchIndex);
+                        for (String data : data2) {
+                            System.out.println(data);
+                        }
+                        break;
+
+                    case 3:
+                        exit = true;
+                        break;
+
+                    default:
+                        System.out.println("Opción no válida. Inténtalo de nuevo.");
+                        break;
+                }
+
+            }while (!exit);
+
+            scanner.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/models/HashTable.java
@@ -0,0 +1,74 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class HashTable {
+    private LinkedList<String>[] table;
+    private int capacity;
+    private HashFunction hashFunction;
+    private HashFunction2 hashFunction2;
+
+    public HashTable(int capacity) {
+        this.capacity = capacity;
+        table = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            table[i] = new LinkedList<>();
+        }
+        hashFunction = new HashFunction();
+        hashFunction2 = new HashFunction2();
+    }
+
+    public void put(String key, String value, int functionNumber) {
+        int index = hashFunction(key, functionNumber);
+        String entry = key + ":" + value;
+        table[index].add(entry);
+    }
+
+    public List<String> getDataAtIndex(int index) {
+        List<String> data = new ArrayList<>();
+        if (index < 0 || index >= capacity) {
+            data.add("Index out of range.");
+            return data;
+        }
+        for (String entry : table[index]) {
+            int separatorIndex = entry.indexOf(':');
+            if (separatorIndex != -1) {
+                String key = entry.substring(0, separatorIndex);
+                String value = entry.substring(separatorIndex + 1);
+                data.add("Key: " + key + ", Value: " + value);
+            } else {
+                data.add("Invalid entry: " + entry);
+            }
+        }
+        return data;
+    }
+
+    public String searchValueByKey(String key, int functionNumber) {
+        String searchKey = key + ":";
+        for (int i = 0; i < capacity; i++) {
+            for (String entry : table[i]) {
+                if (entry.startsWith(searchKey)) {
+                    return "Índice: " + i + ", Valor: " + entry.substring(searchKey.length());
+                }
+            }
+        }
+        return null;
+    }
+
+    private int hashFunction(String key, int functionNumber) {
+        if (functionNumber == 1) {
+            return hashFunction.hash(key) % capacity;
+        } else {
+            return hashFunction2.hash(key, capacity);
+        }
+    }
+
+    public long measurePutTime(String key, String value, int functionNumber) {
+        long startTime = System.nanoTime();
+        put(key, value, functionNumber);
+        long endTime = System.nanoTime();
+        return endTime - startTime;
+    }
+}
